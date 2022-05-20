e5ef73fd6345d5a76e86b2d52dae3d2429ebeeca
Panchito <paancho.96@hotmail.com>
2022-05-20 14:10:04 -0400

    first commit

---
+++ gurobysc.py
@@ -0,0 +1,42 @@
+
+
+
+
+import gurobipy as gp
+from gurobipy import GRB
+
+
+city, coverage, cost = gp.multidict({
+    0: [{0,1}, 4.2],
+    1: [{0,1,5}, 6.1],
+    2: [{2,3}, 5.2],
+    3: [{2,3,4}, 5.5],
+    4: [{3,4,5}, 4.8],
+    5: [{1,4,5}, 9.2]
+})
+
+# MIP model Formulation
+m = gp.Model("set_covering1")
+
+# build es una variable binaria que me dirá en qué ciudad construir o no.
+build = m.addVars(len(city), vtype = GRB.BINARY, name="build")
+#is_covered = m.addVars(len(city)), vtype = GRB.BINARY, name="Is_covered"
+
+m.addConstrs((gp.quicksum(build[r] for r in city if r in coverage[t]) >= 1 for t in city), name="Build2Cover")
+#m.addConstr(build.prod(cost) <= budget, name="budget")
+
+m.setObjective(gp.quicksum(build[t] for t in city), GRB.MINIMIZE)
+
+m.update()
+m.write("set_covering1.lp")
+
+m.optimize()
+
+
+# display optimal values of decision variables
+for ct in build.keys():
+    if(abs(build[ct].x) > 1e-6):
+        print(f"\n Construir estación de bomberos en la ciudad: {ct}.")
+
+
+
